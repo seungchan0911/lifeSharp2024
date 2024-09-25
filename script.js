ea4127f150a4aff65aa9d351fc3d54f236324e1e
seungchan0911 <167081641+seungchan0911@users.noreply.github.com>
2024-09-25 16:54:03 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".popup__close").click(function(){
+        $(".popup__background").hide();
+    })
+    $(".popup__btn").click(function(){
+        $(".popup__background").hide();
+    })
+})
